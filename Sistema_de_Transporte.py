306d1d9af74bd57b40c637d425bf99674c7fef08
facundotrueba <facutrueba2004@gmail.com>
2025-05-27 20:06:06 -0300

    estructuramos clases

--- Sistema_de_Transporte.py
+++ Sistema_de_Transporte.py
@@ -1,4 +1,90 @@
-import pickle
-import json
+import csv
+class Nodos:
+    def __init__(self, nombre,capacidad, soporte_modos):
+        self.nombre=nombre
+        self.capacidad = capacidad
+        self.soporte_modos=soporte_modos
+
+class Conexion:
+    tipos = ("fluvial", "aereo", "terrestre","ferroviario")
+    def __init__(self, inicio, destino, tipo, distancia):
+        
+        self.inicio=inicio
+        self.destino=destino #destino de un tramo especifico
+        if tipo not in Conexion.tipos:
+            raise TypeError("El tipo de conexion es incorrecta")
+        self.tipo=tipo #que la via sea una instancia de la clase medio transporte 
+        self.distancia=distancia
+    @staticmethod
+    def validar(tipo,restriccion,num): #tipo, aereo,fluvial,etc,restriccion velocidad max peso max, cantidad de peso max
+        if restriccion == None:
+            pass
+       # else:
+          #  if tipo=="ferroviario":
+            # if velocidad_nominal < num
+           #         raise ValueError("execede la velocidad")
+        
+
+    
+   
+
+class Vehiculo:
+    def __init__(self, tipo, velocidad_nominal, capacidad_carga, costo_fijo, costo_km, costo_kg): #lo de los costos hacer archivo csv CHEQUEAR LUCAS
+        self.tipo=tipo
+        if velocidad_nominal<= 0:
+            ValueError("La velocidad no puede ser negativa")
+        if capacidad_carga<= 0:
+            ValueError("La capacidad de carga no puede ser negativa")
+        self.velocidad_nominal=velocidad_nominal
+        self.capacidad_carga=capacidad_carga
+        self.costo_fijo = costo_fijo
+        self.costo_km = costo_km
+        self.costo_kg = costo_kg
+
+class Automotor(Vehiculo):
+    def __init__(self, tipo, velocidad_nominal, capacidad_carga, costo_fijo, costo_km, costo_kg):
+        super().__init__(tipo, velocidad_nominal, capacidad_carga, costo_fijo, costo_km, costo_kg)
+        
+    
+    
+class Aereo(Vehiculo):
+  def __init__(self, tipo, velocidad_nominal, capacidad_carga, costo_fijo, costo_km, costo_kg):
+      super().__init__(tipo, velocidad_nominal, capacidad_carga, costo_fijo, costo_km, costo_kg)
+      
+      
+
+class Maritimo(Vehiculo):
+    def __init__(self,tipo, velocidad_nominal, capacidad_carga, costo_fijo, costo_km, costo_kg):
+        super().__init__(tipo, velocidad_nominal, capacidad_carga, costo_fijo, costo_km, costo_kg)
 
 
+class Solicitud_Transporte:
+    def __init__(self, id_carga, peso_kg, origen, destino):
+        if peso_kg <= 0: #Validaciones
+            raise ValueError("El peso debe ser mayor a cero.")
+        if origen == destino:
+            raise ValueError("El destino debe ser distinto del origen.")
+        self.id_carga=id_carga
+        self.peso_kg=peso_kg
+        self.origen=origen
+        self.destino=destino
+        
+
+class Itinerario:
+    def __init__(self, secuencia_tramos, costo, tiempo, destino):
+        self.secuencia_tramos=secuencia_tramos
+        self.costo=costo
+        self.tiempo=tiempo
+        self.destino=destino
+        
+        
+        
+def leer_csv(nombre_archivo):
+    datos=[]
+    try:
+        with open(nombre_archivo, mode='r') as file:
+            lector = csv.read(file)
+            for fila in lector:
+                datos.append(fila)
+    except FileNotFoundError:
+        print("Archivo no encontrado")
